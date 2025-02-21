d85d647e3024ae339c427f9f073f5cb991e025ce
Harold Quispe <128565012+HaroldQn@users.noreply.github.com>
2025-02-21 02:46:41 -0500

    update EnvConfig add HOST, PORT

--- src/config/env.config.js
+++ src/config/env.config.js
@@ -3,6 +3,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const { 
+  HOST_DB: host_db = 'localhost',
+  PORT_DB: port_db = 3306,
   NAME_USER: name_user_db = 'root',
   PASSWORD_DB: pass_user_db = '',
   NAME_DB: name_db = 'companydb',
@@ -12,5 +14,7 @@ console.log(name_user_db, pass_user_db, name_db);
 export const EnvConfig = () => ({
   name_user_db,
   pass_user_db,
-  name_db
+  name_db,
+  host_db,
+  port_db
 });
