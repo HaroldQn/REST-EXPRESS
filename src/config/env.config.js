0bc3bd9f9f36d0872434f0b9dc8f69865252f832
Harold Quispe <128565012+HaroldQn@users.noreply.github.com>
2025-02-21 02:38:27 -0500

    update

--- src/config/env.config.js
+++ src/config/env.config.js
@@ -7,6 +7,7 @@ const {
   PASS_USER: pass_user_db = '',
   NAME_DB: name_db = 'companydb',
 } = process.env;
+console.log(name_user_db, pass_user_db, name_db);
 
 export const EnvConfig = () => ({
   name_user_db,
