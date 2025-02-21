7fb8d0b2f24f178d2e1d86cd63033d14b8997632
Harold Quispe <128565012+HaroldQn@users.noreply.github.com>
2025-02-21 02:41:37 -0500

    update conection mysql

--- src/config/env.config.js
+++ src/config/env.config.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 const { 
   NAME_USER: name_user_db = 'root',
-  PASS_USER: pass_user_db = '',
+  PASSWORD_DB: pass_user_db = '',
   NAME_DB: name_db = 'companydb',
 } = process.env;
 console.log(name_user_db, pass_user_db, name_db);
